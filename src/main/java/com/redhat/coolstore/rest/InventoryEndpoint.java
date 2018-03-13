8618f214be2beade34b00321efd898ee5ec6fc78
Daniel B. G. Gonçalves <dbastosg@macbook.local>
2018-03-13 20:40:53 -0300

    WildFly Swarm Project

--- src/main/java/com/redhat/coolstore/rest/InventoryEndpoint.java
+++ src/main/java/com/redhat/coolstore/rest/InventoryEndpoint.java
@@ -1,11 +1,15 @@
 package com.redhat.coolstore.rest;
 
+import java.util.Optional;
+
 import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+
+import org.wildfly.swarm.spi.runtime.annotations.ConfigurationValue;
 
 import com.redhat.coolstore.model.Inventory;
 import com.redhat.coolstore.service.InventoryService;
@@ -16,10 +20,20 @@ public class InventoryEndpoint {
     @Inject
     private InventoryService inventoryService;
 
+    @Inject
+    @ConfigurationValue("stores.closed")
+    private Optional<String> storesClosed;
+
     @GET
     @Path("/{itemId}")
     @Produces(MediaType.APPLICATION_JSON)
     public Inventory getAvailability(@PathParam("itemId") String itemId) {
-        return inventoryService.getInventory(itemId);
+        Inventory i = inventoryService.getInventory(itemId);
+        for (String store : storesClosed.orElse("").split(",")) {
+            if (store.equalsIgnoreCase(i.getLocation())) {
+                i.setQuantity(0);
+            }
+        }
+        return i;
     }
 }
